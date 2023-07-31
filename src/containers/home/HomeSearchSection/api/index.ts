2c71494d5ad89b3740b720d97b2f5593ce96871f
break-jahun <jh.lee@break.co.kr>
2023-07-31 18:31:20 +0900

    Update index.ts

--- src/containers/home/HomeSearchSection/api/index.ts
+++ src/containers/home/HomeSearchSection/api/index.ts
@@ -17,7 +17,7 @@ export type GetGradingOrderByIdResponse = ResponseType<{
 
 export async function getGradingOrderById(id: number) {
   const { data } = await client.get<GetGradingOrderByIdResponse>(
-    `/api/admin/gradings/v2?sortBy=id&sortOrder=DESC&offset=0&limit=10&searchWord=${id}&searchField=id`
+    `/api/admin/gradings/${id}`
   );
 
   return data;
