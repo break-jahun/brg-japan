d9c98b4a57a201ae8808ea86f4aeb5ba39122967
break-jahun <jh.lee@break.co.kr>
2023-08-01 14:18:36 +0900

    Update index.ts

--- src/containers/home/HomeSearchSection/api/index.ts
+++ src/containers/home/HomeSearchSection/api/index.ts
@@ -17,7 +17,7 @@ export type GetGradingOrderByIdResponse = ResponseType<{
 
 export async function getGradingOrderById(id: number) {
   const { data } = await client.get<GetGradingOrderByIdResponse>(
-    `/api/admin/gradings/${id}`
+    `/api/admin/gradings/public/${id}`
   );
 
   return data;
